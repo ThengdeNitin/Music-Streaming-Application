b182564b3721064fb1be89c1773393bb2ca17206
ThengdeNitin <nitinthengde14368@gmail.com>
2025-09-28 16:03:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,36 @@
 import express from "express";
-import cors from 'cors';
-import dotenv from 'dotenv';
+import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./config/mongoDB.js";
 import adminRouter from "./routes/adminRoutes.js";
-import path from 'path'
+import path from "path";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cors())
-
-app.use('/uploads', express.static(path.join(path.resolve(), 'uploads')));
-
-app.use('/api/admin', adminRouter)
-
-app.get('/', (req, res) => {
-  res.send({
-    activateState:true,
-    error:false,
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, 
+    credentials: true,
   })
-})
+);
+
+app.use(express.json());
+
+app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
+
+app.use("/api/admin", adminRouter);
+
+app.get("/", (req, res) => {
+  res.send({
+    activateState: true,
+    error: false,
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server connected to port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
