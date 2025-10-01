92f0880f4e1b2377a2b44a9f362f1657decaaceb
ThengdeNitin <nitinthengde14368@gmail.com>
2025-10-01 11:24:23 +0530

    Update SideBar-Admin.jsx

--- frontend/src/components/SideBar-Admin.jsx
+++ frontend/src/components/SideBar-Admin.jsx
@@ -6,22 +6,15 @@ import { NavLink, useNavigate } from "react-router-dom";
 
 export const SideBarAdmin = () => {
   const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate("/list-songs");
-    window.location.reload();
-  };
+  window.location.reload();
 
   return (
     <div className="bg-gradient-to-t from-black to-gray-500 max-h-screen p-2 sm:p-4 flex flex-col items-center">
-      {/* Logo */}
       <img
         src={logo}
         className="mt-2 w-28 sm:w-36 md:w-44 cursor-pointer"
         alt="Logo"
       />
-
-      {/* Navigation */}
       <div className="flex flex-col items-center gap-3 sm:gap-5 mt-6 sm:mt-10 w-full">
         <NavLink
           to={"/"}
@@ -43,17 +36,9 @@ export const SideBarAdmin = () => {
           to={"/list-songs"}
           className="flex items-center gap-2 sm:gap-3 text-white text-xs sm:text-sm md:text-base font-medium rounded-xl cursor-pointer hover:bg-gray-700 py-2 px-3 sm:py-3 sm:px-4 w-full"
         >
-          <IoIosAddCircle className="w-6 h-6 sm:w-8 sm:h-8" />
-          <p>list-songs</p>
-        </NavLink>
-
-        {/* <div
-          onClick={handleClick}
-          className="flex items-center gap-2 sm:gap-3 text-white text-xs sm:text-sm md:text-base font-medium rounded-xl cursor-pointer hover:bg-gray-700 py-2 px-3 sm:py-3 sm:px-4 w-full"
-        >
           <IoMdMusicalNote className="w-6 h-6 sm:w-8 sm:h-8" />
           <p>List Of Songs</p>
-        </div> */}
+        </NavLink>
       </div>
     </div>
   );
