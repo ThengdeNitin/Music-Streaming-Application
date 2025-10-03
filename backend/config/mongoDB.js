2d9c448ee62f5d830f9b387cadff799fc65821ba
ThengdeNitin <nitinthengde14368@gmail.com>
2025-10-03 14:25:33 +0530

    Update mongoDB.js

--- backend/config/mongoDB.js
+++ backend/config/mongoDB.js
@@ -1,16 +1,9 @@
-import connectDB from "../../config/mongoDB.js";
-import Song from "../../models/Song.js"; 
+import mongoose from "mongoose";
 
-export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+const connectDB = async () => {
+  mongoose.connection.on("connected", () => console.log('Database Connected'))
 
-  try {
-    await connectDB(); 
-    const songs = await Song.find({});
-    res.status(200).json({ songs });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  await mongoose.connect(`${process.env.MONGODB_URI}/music`)
 }
+
+export default connectDB
