213c2761c1bd8238c75bd01f9205bb3f858dfd3d
ThengdeNitin <nitinthengde14368@gmail.com>
2025-10-03 14:18:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
-import cors from 'cors';
-import dotenv from 'dotenv';
+import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./config/mongoDB.js";
 import adminRouter from "./routes/adminRoutes.js";
-import path from 'path';
+import path from "path";
 
 dotenv.config();
 connectDB();
@@ -11,18 +11,22 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigin =  process.env.FRONTEND_URL; 
-app.use(cors({
-  origin: allowedOrigin,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true
-}));
+const allowedOrigin = process.env.FRONTEND_URL || "*";
+
+app.use(
+  cors({
+    origin: allowedOrigin, 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use('/uploads', express.static(path.join(path.resolve(), 'uploads')));
-app.use('/api/admin', adminRouter);
+app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
+app.use("/api/admin", adminRouter);
 
-app.get('/', (req, res) => {
+
+app.get("/", (req, res) => {
   res.send({
     activateState: true,
     error: false,
